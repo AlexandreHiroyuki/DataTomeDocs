da8cddb5ad6838ade5ffcd1d4c4acaa8ff8b5ce6
AlexandreHiroyuki <alex.hiroyuki@outlook.com>
2024-03-17 18:49:45 +0000

    deploy: 5bb2ca212d7a2a485a17c34b0208cb5e1181cf7b

---
+++ assets/js/fb5d4593.e2769b3e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmoving_average_plus_docs=self.webpackChunkmoving_average_plus_docs||[]).push([[349],{1966:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
